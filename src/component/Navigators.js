0a61f7ac1a46057d328ce8a26bca720e395640c1
Boa <boa.matule@gmail.com>
2020-02-23 21:28:32 +0100

    Installs @expo/vector-icons package and uses the MaterialCommunityIcons; Injects screenOptions props of the <Tab.Navigator>.

--- src/component/Navigators.js
+++ src/component/Navigators.js
@@ -1,23 +1,48 @@
 import React, { PureComponent } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useIsFocused } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Bookmark from '../screens/Bookmark.js';
 import Categories from '../screens/Categories.js';
-import Settings from '../screens/Settings.js'
-import HomeStack from '../component/HomeStack.js'
+import Settings from '../screens/Settings.js';
+import HomeStack from '../component/HomeStack.js';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 
 export default function Navigator() {
   const Tab = createBottomTabNavigator()
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeStack} />
-        <Tab.Screen name="Categories" component={Categories} />
-        <Tab.Screen name="Bookmark" component={Bookmark} />
-        <Tab.Screen name="Settings" component={Settings} />
-      </Tab.Navigator>
+      <Tab.Navigator screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+        if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+        } else if (route.name === 'Bookmark') {
+            iconName = focused ? 'bookmark' : 'bookmark-outline';
+        } else if (route.name === 'Categories') {
+            iconName = focused ? 'apps' : 'apps-box';
+        } else if (route.name === 'Settings') {
+            iconName = focused ? 'settings' : 'settings-box';
+        }
+        return (
+          <MaterialCommunityIcons name={iconName} size={size} color={color} />
+          )
+        },
+      })}
+
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+       > 
+      <Tab.Screen name="Home" component={HomeStack} />
+      <Tab.Screen name="Categories" component={Categories} />
+      <Tab.Screen name="Bookmark" component={Bookmark} />
+      <Tab.Screen name="Settings" component={Settings} />
+    </Tab.Navigator>
+
     </NavigationContainer>
   );
 }
+
