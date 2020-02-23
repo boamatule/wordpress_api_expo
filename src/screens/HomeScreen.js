79bf994b0c66d975ecee151973b8c0c46ec66bc9
Boa <boa.matule@gmail.com>
2020-02-23 21:50:51 +0100

    Data is fetching using the WordPress API and wrapped in a card. Display a Title and a Paragraph.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -14,8 +14,30 @@ export class Home extends Component {
   render() {
     return (
       <View>
-        <Button title="Go to single post" onPress={() =>
-        this.props.navigation.navigate('SinglePost')} />
+        <Card 
+          style={{
+            shadowOffset: {width: 5, height: 5},
+            width: '90%',
+            borderRadius: 12,
+            alignSelf: 'center',
+            marginBottom: 10,
+          }}> 
+          <Card.Content>
+            <Title>Blog post</Title>
+              <Card.Cover
+                style={{
+                  width: 350,
+                  height: 190,
+                  alignSelf: 'center',
+                }}
+                source={{
+                  uri:
+                  'https://images.unsplash.com/photo-1580111116173-171ccf58fe3d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+                }}
+              />
+            <Paragraph>Just a blog post</Paragraph>
+          </Card.Content>
+        </Card>
       </View>
     )
   }
