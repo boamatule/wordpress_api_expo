d757c543cedf4df39d12edc9abb35dc704dd94ea
Boa <boa.matule@gmail.com>
2020-02-25 16:09:41 +0100

    Defines a state variable bookmark in the SinglePost.js and adds the Bookmark icon to the screen. WIP

--- src/screens/SinglePost.js
+++ src/screens/SinglePost.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   View,
   ScrollView,
   ActivityIndicator,
   TouchableOpacity,
   Share
-} from 'react-native';
+} from "react-native";
 import {
   Avatar,
   Button,
@@ -13,11 +13,11 @@ import {
   Paragraph,
   List,
   Title
-} from 'react-native-paper';
-import HTML from 'react-native-htmlview';
-import moment from 'moment';
-import ContentPlaceholder from '../component/ContentPlaceholder';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+} from "react-native-paper";
+import HTML from "react-native-htmlview";
+import moment from "moment";
+import ContentPlaceholder from "../component/ContentPlaceholder";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default class SinglePost extends React.Component {
   constructor(props) {
@@ -26,77 +26,103 @@ export default class SinglePost extends React.Component {
     this.state = {
       isloading: true,
       post: [],
+      bookmark: false
     };
   }
 
-componentDidMount() {
-  this.fetchPost();
-}
+  componentDidMount() {
+    this.fetchPost();
+  }
 
-async fetchPost() {
-    let post_id = this.props.route.params.post_id
+  async fetchPost() {
+    let post_id = this.props.route.params.post_id;
     const response = await fetch(
       `https://kriss.io/wp-json/wp/v2/posts?_embed&include=${post_id}`
     );
     const post = await response.json();
     this.setState({
       post: post,
-      isloading: false,
+      isloading: false
     });
   }
 
   onShare = async (title, uri) => {
     Share.share({
       title: title,
-      url: uri,
+      url: uri
     });
   };
 
   render() {
     let post = this.state.post;
     if (this.state.isloading) {
-      return <ContentPlaceholder />
+      return <ContentPlaceholder />;
     }
     return (
       <ScrollView>
         <Card>
           <Card.Content>
             <Title>{post[0].title.rendered}</Title>
-              <List.Item
-                title={`${post[0]._embedded.author[0].name}`} 
-                description={`${post[0]._embedded.author[0].description}`}
-                left={props => {
-                  return (
-                    <Avatar.Image
-                      size={55}
-                      source={{
-                        uri: `${post[0]._embedded.author[0].avatar_urls[96]}`,
-                      }}
-                    />
-                  )
-                }}
-              />
-            <List.Item 
+            <List.Item
               title={`${post[0]._embedded.author[0].name}`}
               description={`${post[0]._embedded.author[0].description}`}
-               right={props => {
+              left={props => {
+                return (
+                  <Avatar.Image
+                    size={55}
+                    source={{
+                      uri: `${post[0]._embedded.author[0].avatar_urls[96]}`
+                    }}
+                  />
+                );
+              }}
+            />
+            <List.Item
+              title={`${post[0]._embedded.author[0].name}`}
+              description={`${post[0]._embedded.author[0].description}`}
+              right={props => {
                 return (
                   <TouchableOpacity
                     onPress={() =>
                       this.onShare(post[0].title.rendered, post[0].link)
-                    }>
+                    }
+                  >
                     <MaterialCommunityIcons name="share" size={30} />
                   </TouchableOpacity>
                 );
               }}
             />
+
             <List.Item
               title={`Published on ${moment(
                 post[0].date,
-                'YYYYMMDD'
+                "YYYYMMDD"
               ).fromNow()}`}
-              />
-              <Paragraph />
+              right={props => {
+                if (this.state.already_bookmark) {
+                  return (
+                    <TouchableOpacity
+                      onPress={() => this.removeBookMark(post[0].id)}
+                    >
+                      <MaterialCommunityIcons name="bookmark" size={30} />
+                    </TouchableOpacity>
+                  );
+                } else {
+                  return (
+                    <TouchableOpacity
+                      onPress={() => this.saveBookMark(post[0].id)}
+                    >
+                      <MaterialCommunityIcons
+                        name="bookmark-outline"
+                        size={30}
+                      />
+                    </TouchableOpacity>
+                  );
+                }
+              }}
+            />
+
+            <Paragraph />
           </Card.Content>
           <Card.Cover source={{ uri: post[0].jetpack_featured_media_url }} />
           <Card.Content>
