2698fb26fe2ca12dfc933c346bdebf94e9796c8b
Boa <boa.matule@gmail.com>
2020-02-27 17:34:05 +0100

    Implements a Contact menu option UI in order to navigate to the Contact screen in the Settings; In the Contact screen, initialiaze the form instance and defines the structure of the form and use it in the render() function. WIP.

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -17,4 +17,42 @@ export default class Contact extends Compoenent {
       submit: false
     };
   }
+
+  render() {
+    const Form = t.form.Form;
+    const ContactForm = t.struct({
+      email: t.String,
+      name: t.String,
+      message: t.String,
+    });
+    return (
+      <View style={styles.container}>
+        <Form from={ContactForm}/>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  constainer: {
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center',
+    },
+    button: {
+    height: 36,
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+  },
+});
--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,6 +1,6 @@
 import React, { Component, useContext } from "react";
 import { Text, View, TouchableOpacity } from "react-native";
-import { List, Switch } from "react-native-paper";
+import { List, Switch, Icon } from "react-native-paper";
 import { ThemeContext } from "../component/ThemeController";
 
 const Settings = () => {
@@ -12,6 +12,13 @@ const Settings = () => {
         left={() => <List.Icon icon="brightness-4" />}
         right={() => <Switch value={theme} onValueChange={toggleTheme} />}
       />
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate('Contact')}>
+        <List.Item
+          title="Contact Us"
+          left={() => <List.Icon icon ="chevron-right" />}
+          />    
+      </TouchableOpacity>
     </View>
   );
 };
